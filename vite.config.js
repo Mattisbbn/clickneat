4e06eebf6f963d3196915d55b687401edc677b78
Mattisbbn <mattisbbn@gmail.com>
2025-04-08 12:16:32 +0200

    update

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,8 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/js/client.js', 'resources/css/client.css'],
+            input: ['resources/css/app.css', 'resources/js/app.js','resources/js/client.js','resources/js/client.js'],
             refresh: true,
         }),
     ],
-    build: {
-        manifest: true, // Cette option est nécessaire pour générer le fichier manifeste
-      },
 });
